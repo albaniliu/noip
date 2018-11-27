0d867e5a64def79ed3d5036eb36873088a646066
liuzhao <liuzhao@mobike.com>
2018-11-27 22:25:18 +0800

    noip 2018 T5

--- main.cpp
+++ main.cpp
@@ -17,163 +17,62 @@ int read() {
     return res * f;
 }
 
-const int N = 5001;
+const int N = 256;
+int M;
 int n, m;
-vector<int> graph[N];
-int low[N];
-int mark[N];
-bool visited[N];
-int cnt;
-bool canReturn;
-vector<int> res;
-stack<int> nextStack;
-int parent[N];
-int circle[N][2];
-int circleSize;
-set<int> deleted;
+int mod = 1000000007;
 
-void dfs(int cur, int p) {
-    mark[cur] = cnt++;
-//    low[cur] = cnt++;
-//
-//    for (int i = 0; i < graph[cur].size(); i++) {
-//        int next = graph[cur][i];
-//        if (mark[next] == -1) {
-//            dfs(next, cur);
-//            low[cur] = min(low[cur], low[next]);
-//        } else if (next != parent) {
-//            low[cur] = min(low[cur], mark[next]);
-//        }
-//    }
+bool contain(int set, int i);
 
-    parent[cur] = p;
-    for (int i = 0; i < graph[cur].size(); i++) {
-        int next = graph[cur][i];
-        if (next == p) continue;
-        if (mark[next] != -1 && circleSize == 0) {
-            circle[circleSize][0] = cur;
-            circle[circleSize++][1] = next;
-            int np = p;
-            int ncur = cur;
-            while (np != next && np != -1) {
-                circle[circleSize][0] = ncur;
-                circle[circleSize++][1] = np;
-                ncur = np;
-                np = parent[np];
-            }
-            if (np != next) {
-                ncur = next;
-                np = parent[next];
-                while (np != -1) {
-                    circle[circleSize][0] = ncur;
-                    circle[circleSize++][1] = np;
-                    ncur = np;
-                    np = parent[np];
-                }
-            }
-        } else {
-            if (mark[next] == -1)
-                dfs(next, cur);
+void myMul(long long res[][N], long long A[][N], long long B[][N]) {
+    for (int i = 0; i < M; i++) for (int j = 0; j < M; j++) {
+            long long s = 0;
+            for (int k = 0; k < M; k++) s += A[i][k] * B[k][j];
+            res[i][j] = s % mod;
         }
-    }
 }
 
-//void solve(int cur) {
-//    visited[cur] = true;
-//    res.push_back(cur);
-//    vector<int> child;
-//    for (int i = 0; i < graph[cur].size(); i++) {
-//        int next = graph[cur][i];
-//        if (!visited[next]) child.push_back(next);
-//    }
-//    for (int i = 0; i < child.size(); i++) {
-//        int next = child[i];
-//        if (visited[next]) continue;
-//        if (!nextStack.empty() && next < nextStack.top() && canReturn && low[next] < mark[cur] && i == child.size() - 1) {
-//            canReturn = false;
-//            return;
-//        }
-//        if (i < child.size() - 1) nextStack.push(child[i+1]);
-//        solve(next);
-//        if (i < child.size() - 1) nextStack.pop();
-//    }
-//}
-
-void solve(int cur, vector<int>& list) {
-    visited[cur] = true;
-    list.push_back(cur);
-    for (int i = 0; i < graph[cur].size(); i++) {
-        int next = graph[cur][i];
-        if (deleted.find(next) != deleted.end() && deleted.find(cur) != deleted.end()) continue;
-        if (!visited[next]) solve(next, list);
+void myPow(long long res[][N], long long table[][N], int m) {
+    if (m == 1) {
+        for (int i = 0; i < M; i++) for (int j = 0; j < M; j++) res[i][j] = table[i][j] % mod;
+        return;
+    }
+    long long tmp[N][N];
+    myPow(tmp, table, m / 2);
+    for (int i = 0; i < M; i++) for (int j = 0; j < M; j++) {
+        res[i][j] = tmp[i][j];
+        tmp[i][j] = 0;
+    }
+    myMul(tmp, res, res);
+    for (int i = 0; i < M; i++) for (int j = 0; j < M; j++) {
+        res[i][j] = tmp[i][j];
+        tmp[i][j] = 0;
+    }
+    if (m % 2 == 1) {
+        myMul(tmp, res, table);
+        for (int i = 0; i < M; i++) for (int j = 0; j < M; j++){
+            res[i][j] = tmp[i][j];
+            tmp[i][j] = 0;
+        }
     }
 }
 
 int main() {
-    n = read();
-    m = read();
-    for (int i = 0; i < m; i++) {
-        int u = read() - 1;
-        int v = read() - 1;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-    }
-    for (int i = 0; i < n; i++) {
-        sort(graph[i].begin(), graph[i].end());
-    }
-    for (int i = 0; i < n; i++) mark[i] = -1;
-    dfs(0, -1);
-    canReturn = true;
-    res.push_back(n);
-    if (circleSize == 0) {
-        vector<int> tmp;
-        for (int i = 0; i < n; i++) visited[i] = false;
-        solve(0, tmp);
-        bool ok = true;
-        for (int i = 0; i < tmp.size(); i++) {
-            if (tmp[i] < res[i]) {
-                ok = true;
-                break;
-            } else if (tmp[i] > res[i]) {
-                ok = false;
-                break;
-            }
-        }
-        if (ok) {
-            res.clear();
-            for (int i = 0; i < tmp.size(); i++) res.push_back(tmp[i]);
-        }
-    } else {
-        for (int k = 0; k < circleSize; k++) {
-            int u = circle[k][0];
-            int v = circle[k][1];
-            deleted.insert(u);
-            deleted.insert(v);
-            vector<int> tmp;
-            for (int i = 0; i < n; i++) visited[i] = false;
-            solve(0, tmp);
-            deleted.clear();
+    cin>>n>>m;
 
-            if (tmp.size() != n) continue;
-            bool ok = true;
-            for (int i = 0; i < tmp.size(); i++) {
-                if (tmp[i] < res[i]) {
-                    ok = true;
-                    break;
-                } else if (tmp[i] > res[i]) {
-                    ok = false;
-                    break;
-                }
-            }
-            if (ok) {
-                res.clear();
-                for (int i = 0; i < tmp.size(); i++) res.push_back(tmp[i]);
-            }
-        }
+    if (n > m) {
+        int t = n;
+        n = m;
+        m = t;
     }
-    for (int i = 0; i < res.size(); i++) {
-        printf("%d", res[i] + 1);
-        if (i != res.size() - 1) printf(" ");
+    M = (1 << n) | ((n-1) << n);
+    for (int k = 0; k < n; k++) {
+        
     }
-    printf("\n");
+    return 0;
+}
+
+
+bool contain(int set, int i) {
+    return (set & (1 << i)) > 0;
 }
